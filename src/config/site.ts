0c0ca2355355ab03ad6507460c4e95de003d5897
Nizar Fazari <nizarfazari889@gmail.com>
2025-08-02 10:48:28 +0700

    new

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,21 +1,17 @@
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://code-joki.web.id';
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://jokigofast.vercel.app';
 
 export const siteConfig = {
-  title: 'CodeJoki - Jasa Pemrograman dan Tugas Coding Profesional',
+  title: 'JokiGoFast - Jasa Joki Tugas',
   description:
-    'Jasa pembuatan proyek dan tugas pemrograman berkualitas tinggi dengan harga terjangkau. Solusi cepat dan hasil terbaik untuk semua kebutuhan coding Anda.',
+    'Jasa pengerjaan tugas pemrograman yang berkualitas tinggi dengan harga terjangkau. Solusi cepat dan hasil terbaik untuk semua kebutuhan tugas Anda.',
   keyword: [
-    'joki coding',
-    'jasa pemrograman',
-    'bantuan coding',
-    'tugas programming',
-    'proyek IT',
-    'jasa website',
-    'coding assistance',
-    'jasa pembuatan website',
-    'joki tugas kuliah IT',
-    'pengerjaan tugas programming',
-    'ahli coding Indonesia',
+    'joki tugas',
+    'jasa tugas',
+    'bantuan tugas',
+    'tugas kuliah',
+    'proyek kuliah',
+    'jasa ospek',
+    'joki tugas kuliah ospek',
   ],
   url: SITE_URL,
   openGraph: `${SITE_URL}/og.jpg`,
@@ -34,9 +30,9 @@ export const siteConfig = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'CodeJoki - Jasa Pemrograman dan Tugas Coding Profesional',
+    title: 'JokiGoFast - Jasa Joki Tugas',
     description:
-      'Jasa pembuatan proyek dan tugas pemrograman berkualitas tinggi dengan harga terjangkau.',
+      'Jasa tugas yang berkualitas tinggi dengan harga terjangkau.',
     creator: '@side__id',
     images: [`${SITE_URL}/og.jpg`],
   },
